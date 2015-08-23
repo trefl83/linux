364d4ab103396d6e720aefccb0aacc8f32f5a111
trefl83 <trefl83@gmail.com>
2015-08-23 15:45:51 +0200

    minor changes

---
+++ xterm/xterm_options.sh
@@ -0,0 +1,21 @@
+cat >> ~/.Xresources << EOF
+
+! xterm ----------------------------------------------------------------------
+
+xterm*VT100.geometry:     80x25
+xterm*faceName:           Terminus:style=Regular:size=11
+xterm*font:              -*-dina-medium-r-*-*-16-*-*-*-*-*-*-*
+xterm*dynamicColors:      true
+xterm*utf8:               2
+xterm*eightBitInput:      true
+xterm*saveLines:          512
+xterm*scrollKey:          true
+xterm*scrollTtyOutput:    false
+xterm*scrollBar:          true
+xterm*rightScrollBar:     true
+xterm*jumpScroll:         true
+xterm*multiScroll:        true
+xterm*toolBar:            false
+xterm*visualBell:         true
+
+EOF
