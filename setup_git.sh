6e8f3a9240159812ed97a5e76af481c70192055e
trefl83 <trefl83@gmail.com>
2015-08-28 23:52:50 +0200

    udate git setup

--- setup_git.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "trefl83"
-git config --global user.email "trefl83@gmail.com"
---
+++ setup_github.sh
@@ -0,0 +1,4 @@
+git config --global user.name "trefl83"
+git config --global user.email "trefl83@gmail.com"
+git config --global credential.helper 'cache --timeout=3600'
+git config --global color.ui true
