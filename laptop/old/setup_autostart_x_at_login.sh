db4a257264305ef46e2a1f3409aa67c0a8ce742e
trefl83 <trefl83@gmail.com>
2016-02-09 20:51:22 +0100

    update

--- laptop/old/setup_autostart_x_at_login.sh
+++ laptop/old/setup_autostart_x_at_login.sh
@@ -1,4 +1,4 @@
-# startx at login without desktop manger like lightdm or gdm
+# startx at login without display manger like lightdm or slim
 # https://wiki.archlinux.org/index.php/Xinitrc#Autostart_X_at_login
 
 cat >> ~/.bash_profile << EOF
